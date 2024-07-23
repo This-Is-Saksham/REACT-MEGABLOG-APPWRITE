879c693e7fde6c1a9cc871ee20367b4b8cc05da0
This-Is-Saksham <its.sakshamsrivastava@gmail.com>
2024-07-23 11:30:49 +0530

    add routes

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
-import authenticationService from '../../appwrite/auth'
+import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
-        authenticationService.logout().then(() => {
+        authService.logout().then(() => {
             dispatch(logout())
         })
     }
